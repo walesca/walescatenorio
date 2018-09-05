c273c5c2c62243f028a1026e335528360d08fafe
walesca <walescaa.oliveeira@gmail.com>
2018-09-05 14:19:06 -0300

    Update Algoritmo_de_Euclides.py

--- Fun-es recursivas../Algoritmo_de_Euclides.py
+++ Fun-es recursivas../Algoritmo_de_Euclides.py
@@ -1,1 +1,19 @@
 
+
+def euclides(dividendo, divisor):
+    if divisor == 0:
+        return dividendo
+    else:
+        return euclides(divisor, dividendo % divisor)
+
+
+qtd = int(input())
+i=1
+
+while(i<=qtd):
+   A,D = map(int, raw_input().split())
+   print "MDC("+str(A)+","+str(D)+") =", euclides(A,D)
+   i+=1
+
+
+
